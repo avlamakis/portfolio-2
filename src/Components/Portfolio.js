57c6bd3e2e7a8a25ff52a8332fab4a35ce327640
alexvlamakis <alexvlamakis@gmail.com>
2022-03-20 00:29:24 -0600

    file restructure, allow user input for email, design redone with background and color, work readded, fix heroku error

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,36 +1,48 @@
 import React, { Component } from 'react';
-export default class Porfolio extends Component {
+
+class Portfolio extends Component {
   render() {
-    let resumeData = this.props.resumeData;
-    return (
-      <section id="portfolio">
-      <div className="row">
-        <div className="twelve columns collapsed">
-          <h1>Check Out Some of My Work.</h1>
-          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-          {
-            resumeData.portfolio && resumeData.portfolio.map((item)=>{
-              return(
-                <div className="columns portfolio-item">
-                  <div className="item-wrap">
-                    <a href="#modal-01">
-                      <img src={`${item.imgurl}`} className="item-img"/>
-                      <div className="overlay">
-                        <div className="portfolio-item-meta">
-                          <h5>{item.name}</h5>
-                          <p>{item.description}</p>
-                        </div>
-                      </div>
-                    </a>
+
+    if (this.props.data) {
+      var projects = this.props.data.projects.map(function (projects) {
+        var projectImage = 'images/portfolio/' + projects.image;
+        
+        return (
+          <div key={projects.title} className="columns portfolio-item">
+            <div className="item-wrap">
+              <a href={projects.url} target="_blank" title={projects.title}>
+                <img alt={projects.title} src={projectImage} />
+                <div className="overlay">
+                  <div className="portfolio-item-meta">
+                    <h5>{projects.title}</h5>
+                    <p>{projects.category}</p><br />
+                    <p>{projects.info}</p>
                   </div>
                 </div>
-              )
-            })
-          }
+              </a>
+            </div>
+          </div>
+        )
+      })
+    }
+
+    return (
+      <section id="portfolio">
+
+        <div className="row">
+
+          <div className="twelve columns collapsed">
+
+            <h1>PROJECTS</h1>
+
+            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+              {projects}
+            </div>
           </div>
         </div>
-      </div>
-  </section>
-        );
+      </section>
+    );
   }
 }
+
+export default Portfolio;
